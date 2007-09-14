d83a27649c5aea3c0e359ed24a8e340aafb396cc
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2007-09-14 15:12:45 +0000

    added golden ticket generators added sendjdf servlet cleanup
    git-svn-id: http://svn.cip4.org/svnroot/trunk@2580 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ src/java/org/cip4/JDFUtility/SendJDFServlet.java
@@ -0,0 +1,260 @@
+
+package org.cip4.JDFUtility;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadBase;
+import org.cip4.jdflib.core.JDFDoc;
+import org.cip4.jdflib.core.JDFParser;
+import org.cip4.jdflib.core.KElement;
+import org.cip4.jdflib.core.XMLDoc;
+import org.cip4.jdflib.core.JDFElement.EnumVersion;
+
+/**
+ *
+ * @author  rainer
+ *
+ *
+ * @web:servlet-init-param	name="" 
+ *									value=""
+ *									description=""
+ *
+ * @web:servlet-mapping url-pattern="/FixJDFServlet"
+ */
+public class SendJDFServlet extends HttpServlet {
+
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -8902151736245089036L;
+
+
+    /** Initializes the servlet.
+     */
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+    }
+
+    /** Destroys the servlet.
+     */
+    public void destroy() {
+//      foo		
+    }
+
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    {
+        // foo
+    }
+
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+    {
+        System.out.println();
+        System.out.println("Processing request...");
+
+        // Check that we have a file upload request
+        boolean isMultipart = FileUploadBase.isMultipartContent(request);
+        if (isMultipart)
+        {
+            System.out.println("Processing multipart request...");
+            processMultipartRequest(request, response);
+        }
+    }
+
+
+    /**
+     * Parses a multipart request.
+     */
+    private void processMultipartRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+    {
+        List fileItems = JDFServletUtil.getFileList(request);
+
+        FileItem fileItem=null;
+        EnumVersion version=null;
+        int nFiles=0;
+        String urlToSend=null;
+        for(int i=0;i<fileItems.size();i++)
+        {
+            Runtime.getRuntime().gc(); // clean up before loading
+            FileItem item = (FileItem) fileItems.get(i);
+            if(item.isFormField())
+            {
+                System.out.println("Form name: " + item.getFieldName());
+                if(item.getFieldName().equals("sendURL"))
+                {
+                    urlToSend = item.getString();
+                }        
+            }
+            else if (item.getSize()<20 || item.getName().length()==0)
+            {
+                System.out.println("Bad File name: " + item.getName());
+            }
+            else // ok
+            {
+                System.out.println("File name: " + item.getName());
+                fileItem=item;
+                nFiles++;
+            }
+        }
+
+        System.out.println("File count: " + nFiles);
+        if(fileItem!=null)
+        {
+            // Get the first file item
+            // To do: Process all file items
+            System.out.println("File size: " + fileItem.getSize()/1024 + "KB");
+            System.out.println("File type: " + fileItem.getContentType());
+        }
+
+        XMLDoc htmlDoc=new XMLDoc("html",null);
+        KElement html=htmlDoc.getRoot();
+        html.appendElement("LINK").setAttribute("HREF","http://www.cip4.org/css/styles_pc.css");
+        html.getElement("LINK").setAttribute("TYPE","text/css");
+        html.getElement("LINK").setAttribute("REL","stylesheet");
+
+        html.appendElement("head").appendElement("title").appendText("SendJDF "+urlToSend+" output");
+//      html.appendXMLComment("#include virtual=\"/global/navigation/menue_switch.php?section=support\" ");
+        html.appendElement("H1").appendText("SendJDF Response");
+        html.appendElement("br");
+        html.appendElement("H2").appendText("SendJDF "+urlToSend+" output");
+
+        System.out.println("try");
+        boolean success=false;
+
+        if(fileItem!=null)
+        {
+            InputStream ins = fileItem.getInputStream();
+            JDFParser p=new JDFParser();
+            JDFDoc d=p.parseStream(ins);
+            if(d!=null && urlToSend!=null)
+            {
+                final URL url         = new URL(urlToSend);
+
+                final HttpURLConnection urlCon = (HttpURLConnection) url.openConnection();
+                urlCon.setDoOutput(true);
+                urlCon.setRequestProperty("Connection", "close");
+                urlCon.setRequestProperty("Content-Type", d.getContentType());
+
+                d.write2Stream(urlCon.getOutputStream(), 0, true);
+                InputStream inStream;
+                try 
+                {
+                    inStream= urlCon.getInputStream();
+                }
+                catch (Exception x)
+                {
+                    inStream=null;
+                }
+                JDFDoc docResp=null;
+                success=urlCon.getResponseCode()==200;             
+                String outFileName=null;
+                if(inStream!=null)
+                {
+                    final JDFParser parser = new JDFParser();
+                    parser.parseStream(inStream); 
+                    docResp= parser.getDocument()==null ? null : new JDFDoc(parser.getDocument());
+                    File outFile=JDFServletUtil.getTmpFile("SendJDFTmp",fileItem,"jdf",".jdf");
+                    if(docResp!=null)
+                    {
+                        docResp.write2File(outFile.getAbsolutePath(), 2, true);
+                        outFileName=outFile.getName();
+                    }
+                }
+
+                System.out.println(success ? "Send was successful" : "Send Failed");
+
+                // very basic html output
+                html.appendElement("H2").appendText("Sent "+fileItem.getName()+" to "+urlToSend);
+                html.appendElement("H3").appendText("Return Code: "+urlCon.getResponseCode());
+                html.appendElement("H3").appendText("Headers:");
+                KElement list = html.appendElement("ul");
+                Map fields=urlCon.getHeaderFields();
+                if(fields!=null)
+                {
+                    Iterator it=fields.keySet().iterator();
+                    while(it.hasNext())
+                    {
+                        String key=(String)it.next();
+                        String value=urlCon.getHeaderField(key);
+                        list.appendElement("li").setText(key+": "+value);
+                    }
+                }
+
+                if(success)
+                {
+                    if(outFileName!=null)
+                    {
+                        html.appendElement("H2").setText("Returned document");
+                        KElement dl=html.appendElement("a");
+                        dl.appendText(outFileName);
+                        dl.setAttribute("href","./SendJDFTmp/"+outFileName,null);
+                        html.appendElement("hr");
+                        html.appendElement("pre").appendElement("code").setText(docResp.write2String(2));
+                        html.appendElement("hr");
+                    }
+                    else
+                    {
+                        html.appendElement("H3").appendText("No Response Stream was received");
+                    }
+                }
+                else
+                {
+                    KElement e=html.getCreateXPathElement("H2/Font");
+                    e.setAttribute("color","xff0000");
+                    e.appendText("Sending of " +fileItem.getName()+ " to "+urlToSend+" failed!!! ");                
+                }
+                JDFServletUtil.cleanup("SendJDFTmp");
+
+                fileItem.delete();
+            }
+            else
+            {
+                html.appendText("No file to send to  "+urlToSend+". Submission failed!!! ");                                
+            }
+        }
+
+        //       html.appendXMLComment("#include virtual=\"/global/navigation/menue_switch.php?section=support\" ");
+        // Writes the XMP packet to output
+        // Todo: Use JSP instead of writing directly to output
+
+        response.setContentType("text/html;charset=utf-8");
+
+        PrintWriter out = response.getWriter();
+        out.println(htmlDoc.write2String(0));
+
+    }
+
+
+    /** Returns a short description of the servlet.
+     */
+    public String getServletInfo() {
+        return "FixJDF Servlet";
+    }
+
+}
