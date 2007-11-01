32905246cc90095bdb2981882d44f5d04d0fef8c
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2007-11-01 17:12:12 +0000

    Bambi - minor cleanup to allow extension of BambiNSExtension C++ Streams enable buffered streams that take ownership of their children alces - add cast for VElemet -> List<JDFType> where necessary
    git-svn-id: http://svn.cip4.org/svnroot/trunk@2836 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/java/org/cip4/JDFUtility/DumpJDFServlet.java
+++ src/java/org/cip4/JDFUtility/DumpJDFServlet.java
@@ -2,10 +2,12 @@
 package org.cip4.JDFUtility;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import javax.mail.Multipart;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.lf5.util.StreamUtils;
+import org.cip4.jdflib.util.MimeUtil;
+import org.cip4.jdflib.util.StringUtil;
 
 /**
  *
@@ -26,28 +30,28 @@ import org.apache.log4j.lf5.util.StreamUtils;
  * @web:servlet-mapping url-pattern="/FixJDFServlet"
  */
 public class DumpJDFServlet extends HttpServlet {
-    
-    
+
+
     /**
      * 
      */
     private static final long serialVersionUID = -8902151736245089036L;
-    
-    
+
+
     /** Initializes the servlet.
      */
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        
+
     }
-    
+
     /** Destroys the servlet.
      */
     public void destroy() {
 //      foo		
     }
-    
-    
+
+
     /** 
      * Handles all HTTP <code>GET / POST etc.</code> methods.
      * @param request servlet request
@@ -56,38 +60,59 @@ public class DumpJDFServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response)
     {
         System.out.println("dump service");
-        
+
         File f=JDFServletUtil.getTmpFile("dump", (String)null, "d", ".txt");
-        
+
         try
         {
-        FileOutputStream fs=new FileOutputStream(f);
-        PrintWriter w=new PrintWriter(fs);
-//        w.println("Context Type:"+http);
-        w.println("Context Path:"+request.getContextPath());
-        w.println("Context Type:"+request.getContentType());
-        w.println("-------------------------------------------------");
-        w.flush();
-        StreamUtils.copyThenClose(request.getInputStream(), fs);
-        OutputStream os=response.getOutputStream();
-        w=new PrintWriter(os);
-        w.print("<HTML><HEAD><TITLE>JDF Test DUMP</TITLE></HEAD>");
-        w.print("<H1>Request Dump</H1><Body>");
-        w.println("Context Path:"+request.getContextPath()+"<BR/>");
-        w.println("Context Type:"+request.getContentType()+"<BR/>");
-        w.print("</Body></HTML>");
-        w.flush();
-       }
+
+            FileOutputStream fs=new FileOutputStream(f);
+            PrintWriter w=new PrintWriter(fs);
+//          w.println("Context Type:"+http);
+            w.println("Context Path:"+request.getContextPath());
+
+            final String contentType = request.getContentType();
+            w.println("Context Type:"+contentType);
+            w.println("Context Length:"+request.getContentLength());
+            w.print("-------------------------------------------------!>\n");
+            w.flush();
+
+            StreamUtils.copyThenClose(request.getInputStream(), fs);
+            OutputStream os=response.getOutputStream();
+            w=new PrintWriter(os);
+            w.print("<HTML><HEAD><TITLE>JDF Test DUMP</TITLE></HEAD>");
+            w.print("<H1>Request Dump</H1><Body>");
+            w.println("Context Path:"+request.getContextPath()+"<BR/>");
+            if(contentType!=null)
+                w.println("Content Type:"+contentType+"<BR/>");
+            w.print("</Body></HTML>");
+            w.flush();
+
+            if(contentType!=null && contentType.toLowerCase().startsWith("multipart/related"))
+            {
+                FileInputStream fis=new FileInputStream(f);
+                final String dirName = StringUtil.newExtension(f.getPath(),".dir");
+                System.out.println("dump mime: "+dirName);
+                char c='a';
+                while(c!='!')
+                    c=(char)fis.read();
+                fis.read();
+                fis.read();
+
+                Multipart mp=MimeUtil.getMultiPart(fis);
+                MimeUtil.writeToDir(mp, new File(dirName));
+            }
+        }
         catch (Exception e) {
             System.out.println("dump service - snafu: "+e);
-       }
-         
+        }
+
     }
-    
+
     /** Returns a short description of the servlet.
      */
     public String getServletInfo() {
         return "DumpJDF Servlet";
     }
-    
+
 }
