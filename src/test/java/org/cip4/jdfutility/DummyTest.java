a7b823a8d42a9e822ebd5ed6cb2aee77a4b3e29a
s.meissner <s.meissner@flyeralarm.de>
2014-09-03 12:50:45 +0200

    Implementation of a dummy unit test.

---
+++ src/test/java/org/cip4/jdfutility/DummyTest.java
@@ -0,0 +1,21 @@
+package org.cip4.jdfutility;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Just a simple dummy test.
+ */
+public class DummyTest {
+
+    @Test
+    public void testDummy() {
+
+        // arrange
+
+        // act
+        Assert.assertTrue(true);
+
+        // assert
+    }
+}
