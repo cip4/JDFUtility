5f77d9fa967cf2f9d56b41a1b1d011b5c855f7ea
Rainer Prosi <rainer.prosi@heidelberg.com>
2021-01-05 16:42:06 +0100

    missing test update

--- src/test/java/org/cip4/jdfutility/FixJDFServletTest.java
+++ src/test/java/org/cip4/jdfutility/FixJDFServletTest.java
@@ -8,6 +8,7 @@ import org.cip4.jdflib.core.ElementName;
 import org.cip4.jdflib.core.JDFDoc;
 import org.cip4.jdflib.core.JDFElement.EnumVersion;
 import org.cip4.jdflib.extensions.XJDFHelper;
+import org.cip4.jdfutility.CheckJDFServlet.FixCall;
 import org.junit.Test;
 
 public class FixJDFServletTest extends JDFUtilityTestBase
@@ -17,7 +18,8 @@ public class FixJDFServletTest extends JDFUtilityTestBase
 	public void testXX()
 	{
 		final XJDFHelper h = new XJDFHelper(EnumVersion.Version_2_0, "J");
-		final FixJDFServlet s = new FixJDFServlet();
+		final CheckJDFServlet checkJDFServlet = new CheckJDFServlet();
+		final FixCall s = checkJDFServlet.new FixCall(checkJDFServlet, null, null);
 		final JDFDoc d1 = s.updateSingle(EnumVersion.Version_2_1, new JDFDoc(h.getRoot().getOwnerDocument()));
 		assertNotNull(XJDFHelper.getHelper(d1));
 	}
@@ -26,7 +28,8 @@ public class FixJDFServletTest extends JDFUtilityTestBase
 	public void testXJ()
 	{
 		final XJDFHelper h = new XJDFHelper(EnumVersion.Version_2_0, "J");
-		final FixJDFServlet s = new FixJDFServlet();
+		final CheckJDFServlet checkJDFServlet = new CheckJDFServlet();
+		final FixCall s = checkJDFServlet.new FixCall(checkJDFServlet, null, null);
 		final JDFDoc d1 = s.updateSingle(EnumVersion.Version_1_6, new JDFDoc(h.getRoot().getOwnerDocument()));
 		assertNull(XJDFHelper.getHelper(d1));
 		assertEquals(EnumVersion.Version_1_6, d1.getJDFRoot().getVersion(true));
@@ -36,7 +39,8 @@ public class FixJDFServletTest extends JDFUtilityTestBase
 	public void testJJ()
 	{
 		final JDFDoc d0 = new JDFDoc(ElementName.JDF);
-		final FixJDFServlet s = new FixJDFServlet();
+		final CheckJDFServlet checkJDFServlet = new CheckJDFServlet();
+		final FixCall s = checkJDFServlet.new FixCall(checkJDFServlet, null, null);
 		final JDFDoc d1 = s.updateSingle(EnumVersion.Version_1_6, d0);
 		assertNull(XJDFHelper.getHelper(d1));
 		assertEquals(EnumVersion.Version_1_6, d1.getJDFRoot().getVersion(true));
@@ -46,7 +50,8 @@ public class FixJDFServletTest extends JDFUtilityTestBase
 	public void testJX()
 	{
 		final JDFDoc d0 = new JDFDoc(ElementName.JDF);
-		final FixJDFServlet s = new FixJDFServlet();
+		final CheckJDFServlet checkJDFServlet = new CheckJDFServlet();
+		final FixCall s = checkJDFServlet.new FixCall(checkJDFServlet, null, null);
 		final JDFDoc d1 = s.updateSingle(EnumVersion.Version_2_1, d0);
 		assertNotNull(XJDFHelper.getHelper(d1));
 	}
