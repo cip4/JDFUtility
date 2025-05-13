4d3471245321bfce6e349d94500843b56dfc3c7e
Rainer Prosi <rainer.prosi@heidelberg.com>
2025-05-13 19:55:40 +0200

    test loosening

--- src/test/java/org/cip4/jdfutility/server/JettyServerTest.java
+++ src/test/java/org/cip4/jdfutility/server/JettyServerTest.java
@@ -73,7 +73,7 @@ class JettyServerTest extends JDFUtilityTestBase
 		final HTTPDump ns = new HTTPDump();
 		ns.setPort(getPort());
 		final int t = Thread.activeCount();
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 10; i++)
 		{
 			assertTrue(ns.tryStart());
 			while (!ns.isStarted())
@@ -147,6 +147,12 @@ class JettyServerTest extends JDFUtilityTestBase
 		ns.setPort(getPort());
 		assertFalse(ns.isStarted());
 		assertTrue(ns.tryStart());
+		for (int i = 0; i < 42; i++)
+		{
+			if (!ns.isStarted())
+				ThreadUtil.sleep(100);
+		}
+
 		assertTrue(ns.isStarted());
 		ns.stop();
 		assertFalse(ns.isStarted());
@@ -159,6 +165,11 @@ class JettyServerTest extends JDFUtilityTestBase
 		final HTTPDump ns = new HTTPDump();
 		ns.setPort(getPort());
 		ns.start();
+		for (int i = 0; i < 42; i++)
+		{
+			if (!ns.isRunning())
+				ThreadUtil.sleep(100);
+		}
 		assertTrue(ns.isRunning());
 		ns.stop();
 		assertFalse(ns.isStarted());
