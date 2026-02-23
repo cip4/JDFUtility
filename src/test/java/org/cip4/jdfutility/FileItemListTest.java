5e530813548598a72517efabc2064c7b72de201c
Rainer Prosi <rainer.prosi@heidelberg.com>
2026-02-23 13:17:47 +0100

    new jetty

--- src/test/java/org/cip4/jdfutility/FileItemListTest.java
+++ src/test/java/org/cip4/jdfutility/FileItemListTest.java
@@ -3,8 +3,8 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2025 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2026 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +12,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -20,17 +20,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -56,17 +56,17 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.jdfutility;
 
@@ -87,6 +87,7 @@ import java.util.Map;
 
 import org.apache.commons.io.FileCleaningTracker;
 import org.cip4.jdflib.util.ContainerUtil;
+import org.cip4.jdflib.util.UrlUtil;
 import org.junit.jupiter.api.Test;
 
 import jakarta.servlet.ServletException;
@@ -99,62 +100,64 @@ class FileItemListTest extends JDFUtilityTestBase
 	@Test
 	void testString() throws Exception
 	{
-		assertNotNull(new FileItemList(getRequestMock(), 42).toString());
+		assertNotNull(new FileItemList(getRequestMock(null), 42).toString());
 	}
 
 	@Test
 	void testGetBool() throws Exception
 	{
-		assertFalse(new FileItemList(getRequestMock(), 42).getBoolField("a", false));
-		assertTrue(new FileItemList(getRequestMock(), 42).getBoolField("a1", false));
-		assertTrue(new FileItemList(getRequestMock(), 42).getBoolField("A1", false));
+		assertFalse(new FileItemList(getRequestMock(null), 42).getBoolField("a", false));
+		assertTrue(new FileItemList(getRequestMock(null), 42).getBoolField("a1", false));
+		assertTrue(new FileItemList(getRequestMock(null), 42).getBoolField("A1", false));
 	}
 
 	@Test
 	void testGetInt() throws Exception
 	{
-		assertEquals(0, new FileItemList(getRequestMock(), 42).getIntField("a", 0));
-		assertEquals(1, new FileItemList(getRequestMock(), 42).getIntField("a2", 0));
-		assertEquals(1, new FileItemList(getRequestMock(), 42).getIntField("A2", 0));
+		assertEquals(0, new FileItemList(getRequestMock(null), 42).getIntField("a", 0));
+		assertEquals(1, new FileItemList(getRequestMock(null), 42).getIntField("a2", 0));
+		assertEquals(1, new FileItemList(getRequestMock(null), 42).getIntField("A2", 0));
 	}
 
 	@Test
 	void testGetField() throws Exception
 	{
-		assertEquals(null, new FileItemList(getRequestMock(), 42).getField("a"));
-		assertEquals("abc", new FileItemList(getRequestMock(), 42).getField("a3"));
-		assertEquals("abc", new FileItemList(getRequestMock(), 42).getField("A3"));
+		assertEquals(null, new FileItemList(getRequestMock(null), 42).getField("a"));
+		assertEquals("abc", new FileItemList(getRequestMock(null), 42).getField("a3"));
+		assertEquals("abc", new FileItemList(getRequestMock(UrlUtil.GET), 42).getField("A3"));
 	}
 
 	@Test
 	void testGetFile() throws Exception
 	{
-		assertNull(new FileItemList(getRequestMock(), 42).getFile(0));
-		assertNull(new FileItemList(getRequestMock(), 42).getFile("foo"));
+		assertNull(new FileItemList(getRequestMock(null), 42).getFile(0));
+		assertNull(new FileItemList(getRequestMock(null), 42).getFile("foo"));
 	}
 
 	@Test
 	void testGetFileStream() throws Exception
 	{
-		assertNull(new FileItemList(getRequestMock(), 42).getFileInputStream(0));
-		assertNull(new FileItemList(getRequestMock(), 42).getFileInputStream("foo"));
+		assertNull(new FileItemList(getRequestMock(null), 42).getFileInputStream(0));
+		assertNull(new FileItemList(getRequestMock(null), 42).getFileInputStream("foo"));
 	}
 
 	@Test
 	void testGetMem() throws Exception
 	{
-		assertNull(FileItemList.getMemoryFileItemList(getRequestMock(), 999).getFileInputStream(0));
-		assertNull(FileItemList.getFileItemList(getRequestMock(), 999).getFileInputStream(0));
+		assertNull(FileItemList.getMemoryFileItemList(getRequestMock(null), 999).getFileInputStream(0));
+		assertNull(FileItemList.getFileItemList(getRequestMock(UrlUtil.GET), 999).getFileInputStream(0));
 	}
 
-	HttpServletRequest getRequestMock() throws IOException, ServletException
+	HttpServletRequest getRequestMock(String method) throws IOException, ServletException
 	{
 		final HttpServletRequest mock = mock(HttpServletRequest.class);
-		final Map<String, String[]> pm = new HashMap<String, String[]>();
+		final Map<String, String[]> pm = new HashMap<>();
 		pm.put("A1", new String[] { "true" });
 		pm.put("A2", new String[] { "1" });
 		pm.put("A3", new String[] { "abc" });
 		when(mock.getParameterMap()).thenReturn(pm);
+		when(mock.getMethod()).thenReturn(method);
+		when(mock.getContentType()).thenReturn("multipart/form-data; boundary=aaa");
 
 		final Part part1 = mock(Part.class);
 		when(mock.getPart(any())).thenReturn(part1);
@@ -165,7 +168,7 @@ class FileItemListTest extends JDFUtilityTestBase
 	@Test
 	void testGetFactory() throws Exception
 	{
-		final FileItemList l = new FileItemList(getRequestMock(), 42);
+		final FileItemList l = new FileItemList(getRequestMock(null), 42);
 		assertThrows(IllegalArgumentException.class, () -> l.getFactory(true, 0));
 		l.getFactory(false, 0);
 		l.getFactory(false, 99999);
@@ -178,7 +181,7 @@ class FileItemListTest extends JDFUtilityTestBase
 		final FileCleaningTracker tracker = new FileCleaningTracker();
 		FileItemList.setTracker(tracker);
 		assertEquals(tracker, FileItemList.getTracker());
-		assertEquals(tracker, new FileItemList(getRequestMock(), 42).getFactory(true, 42).getFileCleaningTracker());
+		assertEquals(tracker, new FileItemList(getRequestMock(null), 42).getFactory(true, 42).getFileCleaningTracker());
 	}
 
 }
